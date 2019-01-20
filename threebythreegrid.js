03a2073aa5d65eaf04797a316ec9a0793eb25640
mihirdeshpande <mihirdeshpande@users.noreply.github.com>
2019-01-19 19:11:46 -0500

    Update threebythreegrid.js
    Update for resize and formatting

--- threebythreegrid.js
+++ threebythreegrid.js
@@ -11,21 +11,39 @@ export class ThreeByThreeGrid extends React.Component{
 	
 	render(){
 		return (
-			<View style={{flexDirection:'column', backgroundColor:'green'}}>
+			<View style={{flexDirection:'column'}}>
 				<View style={{flexDirection:'row'}}>
+				<View style={{flex:1, flexDirection:'column', justifyContent:'center'}}>
 					<Square ref="0" propTest={this.props.propTest} displayMsg={this.props.displayMsg} gridId={this.props.gridId} squareId='0' sqColor={this.props.gridColor}/>
+				</View>
+				<View style={{flex:1, flexDirection:'column', justifyContent:'center'}}>
 					<Square ref="1" propTest={this.props.propTest} displayMsg={this.props.displayMsg} gridId={this.props.gridId} squareId='1' sqColor={this.props.gridColor}/>
+					</View>
+					<View style={{flex:1, flexDirection:'column', justifyContent:'center'}}>
 					<Square ref="2" propTest={this.props.propTest} displayMsg={this.props.displayMsg} gridId={this.props.gridId} squareId='2' sqColor={this.props.gridColor}/>
 				</View>
-				<View style={{flexDirection:'row'}}>
-					<Square ref="3" propTest={this.props.propTest} displayMsg={this.props.displayMsg} gridId={this.props.gridId} squareId='3' sqColor={this.props.gridColor}/>
-					<Square ref="4" propTest={this.props.propTest} displayMsg={this.props.displayMsg} gridId={this.props.gridId} squareId='4' sqColor={this.props.gridColor}/>
-					<Square ref="5" propTest={this.props.propTest} displayMsg={this.props.displayMsg} gridId={this.props.gridId} squareId='5' sqColor={this.props.gridColor}/>
 				</View>
 				<View style={{flexDirection:'row'}}>
+				<View style={{flex:1, flexDirection:'column', justifyContent:'center'}}>
+					<Square ref="3" propTest={this.props.propTest} displayMsg={this.props.displayMsg} gridId={this.props.gridId} squareId='3' sqColor={this.props.gridColor}/>
+				</View>
+				<View style={{flex:1, flexDirection:'column', justifyContent:'center'}}>
+					<Square ref="4" propTest={this.props.propTest} displayMsg={this.props.displayMsg} gridId={this.props.gridId} squareId='4' sqColor={this.props.gridColor}/>
+					</View>
+					<View style={{flex:1, flexDirection:'column', justifyContent:'center'}}>
+					<Square ref="5" propTest={this.props.propTest} displayMsg={this.props.displayMsg} gridId={this.props.gridId} squareId='5' sqColor={this.props.gridColor}/>
+					</View>
+				</View>
+				<View style={{flexDirection:'row'}}>
+				<View style={{flex:1, flexDirection:'column', justifyContent:'center'}}>
 					<Square ref="6" propTest={this.props.propTest} displayMsg={this.props.displayMsg} gridId={this.props.gridId} squareId='6' sqColor={this.props.gridColor}/>
+					</View>
+					<View style={{flex:1, flexDirection:'column', justifyContent:'center'}}>
 					<Square ref="7" propTest={this.props.propTest} displayMsg={this.props.displayMsg} gridId={this.props.gridId} squareId='7' sqColor={this.props.gridColor}/>
+					</View>
+					<View style={{flex:1, flexDirection:'column', justifyContent:'center'}}>
 					<Square ref="8" propTest={this.props.propTest} displayMsg={this.props.displayMsg} gridId={this.props.gridId} squareId='8' sqColor={this.props.gridColor}/>
+					</View>
 				</View>
 			</View>
 		);
